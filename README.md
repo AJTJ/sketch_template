9373b2aacbd4d98634de647c86138b2b3152b319
AJTJ <janke.aaron@gmail.com>
2022-02-16 16:56:22 -0700

    added frame capturing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Template for starting a sketch with Nannou
+### This uses a template I've set up
 
-### Notes
-`cw` to run
+### Warning!
+#### This project automatically builds all artifacts here: `../nannou_shared_directory`, so if you blindly `cargo build` this sketch on your machine it will create this directory. To avoid this behaviour simply delete the `target-dir= "../nannou_shared_directory"` in `.cargo/config.toml`
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use nannou::prelude::*;
 use rand::prelude::*;
+use std::path::Path;
 
 fn main() {
     nannou::app(model)
@@ -27,6 +28,23 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     // GENERAL
     let draw = app.draw();
 
+    draw.background().color(BLUE);
+
     // DRAW TO FRAME
     draw.to_frame(app, &frame).unwrap();
+
+    // CAPTURE FRAME
+    let file_path = captured_frame_path(app, &frame);
+    app.main_window().capture_frame(file_path);
+}
+
+fn captured_frame_path(app: &App, frame: &Frame) -> std::path::PathBuf {
+    // Create a path that we want to save this frame to.
+    // capture all the frames to a directory outside of the current crate
+    Path::new("../sketch_frames")
+        .join(app.exe_name().unwrap())
+        // Name each file after the number of the frame.
+        .join(format!("{:03}", frame.nth()))
+        // The extension will be PNG. We also support tiff, bmp, gif, jpeg, webp and some others.
+        .with_extension("png")
 }
